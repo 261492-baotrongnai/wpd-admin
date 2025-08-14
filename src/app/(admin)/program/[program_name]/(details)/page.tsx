6c2bd4399a08d5b4b24ff7f2ee0e8f5cde89d25b
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-08-14 21:52:28 +0700

    refactor : enhance program detail layout with infoBox component

--- src/app/(admin)/program/[program_name]/(details)/page.tsx
+++ src/app/(admin)/program/[program_name]/(details)/page.tsx
@@ -1,9 +1,17 @@
 "use server";
 
 import ComponentCard from "@/components/common/ComponentCard";
-import { Organization } from "@/types/organization.type";
 import { ProgramTable } from "@/types/program.type";
 import { cookies } from "next/headers";
+
+const infoBox = (label: string, info: string) => {
+  return (
+    <div className="inline-flex items-center gap-2 w-full">
+      <span className="text-gray-400 w-[30%]">{label}</span>
+      <span className="text-gray-600 w-[70%]">{info}</span>
+    </div>
+  );
+};
 
 export default async function ProgramDetailPage() {
   const cookieStore = await cookies();
@@ -18,10 +26,13 @@ export default async function ProgramDetailPage() {
 
   return (
     <ComponentCard title={`รายละเอียดโครงการ`}>
-      <div>
-        <h2>{detail.program.name}</h2>
-        <p>{detail.program.organization?.thai_name}</p>
-        <p>Code: {detail.program.code}</p>
+      <div className="space-y-6 flex flex-col">
+        {infoBox("Program Name", detail.program.name)}
+        {infoBox(
+          "Organization",
+          detail.program.organization?.thai_name || "N/A"
+        )}
+        {infoBox("Code", detail.program.code)}
       </div>
     </ComponentCard>
   );
